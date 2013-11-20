5ef3d0702e95ffea3bd324a01890842733fa3fd9
N. Harrison Ripps <hripps@redhat.com>
2013-11-20 14:01:54 -0500

    Updated package to clear out old DATA_DIR content

--- update_package.sh
+++ update_package.sh
@@ -2,6 +2,7 @@
 
 cd ../openshift-extras/oo-install && bundle exec rake package
 cd ../../oo/
+ssh 527d40f55973cacf58000175@oo-install.rhcloud.com 'rm -rf $OPENSHIFT_DATA_DIR/*'
 scp -r ../openshift-extras/oo-install/package/* 527d40f55973cacf58000175@oo-install.rhcloud.com:/var/lib/openshift/527d40f55973cacf58000175/app-root/data/
 ssh 527d40f55973cacf58000175@oo-install.rhcloud.com 'gear restart --cart diy'
 exit
